9b35da39be5ed42f1780576cd9bcf2afa628fd90
Alex <jianshengalexng17317@gmail.com>
2025-12-07 19:32:51 +0800

    Add CreateTaskDto and UpdateTaskDto

--- ProjectManagement.API/obj/Debug/net9.0/ProjectManagement.API.AssemblyInfo.cs
+++ ProjectManagement.API/obj/Debug/net9.0/ProjectManagement.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectManagement.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7755f77c8075766004c3ad47a12c729d3b15819c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+794124752de49e0de79725b8cebcf1a877d3ea53")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectManagement.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectManagement.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
