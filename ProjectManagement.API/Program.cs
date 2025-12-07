f9fc9fd617ed045df98782dc86ab37a46b0467f3
katherine101001 <katherine.academic@gmail.com>
2025-12-07 18:53:54 +0800

    Updated Program.cs

--- ProjectManagement.API/Program.cs
+++ ProjectManagement.API/Program.cs
@@ -1,5 +1,9 @@
 using Microsoft.EntityFrameworkCore;
+using ProjectManagement.Application.Interfaces.Repositories;
+using ProjectManagement.Application.Interfaces.Services;
+using ProjectManagement.Application.Services;
 using ProjectManagement.Infrastructure.Data;
+using ProjectManagement.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,9 +11,42 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("ProjectManagementDBConnectionString")));
 
+// Add repositories
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IProjectRepository, ProjectRepository>();
+builder.Services.AddScoped<ITaskRepository, ProjectTaskRepository>();
+builder.Services.AddScoped<ICommentRepository, CommentRepository>();
+builder.Services.AddScoped<INotificationRepository, NotificationRepository>();
+
+// Add services
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IProjectService, ProjectService>();
+builder.Services.AddScoped<IProjectTaskService, ProjectTaskService>();
+builder.Services.AddScoped<ICommentService, CommentService>();
+builder.Services.AddScoped<INotificationService, NotificationService>();
+builder.Services.AddScoped<IDashboardService, DashboardService>();
+
+// Add AutoMapper
+builder.Services.AddAutoMapper(typeof(Program));
+
 // Add controllers
 builder.Services.AddControllers();
 
+// Add Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+
 app.MapControllers();
+
 app.Run();
