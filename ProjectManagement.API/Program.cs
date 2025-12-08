9991400533e58d63e59c635670399b83ca4a8530
katherine101001 <katherine.academic@gmail.com>
2025-12-08 17:14:07 +0800

    Done Create Project API

--- ProjectManagement.API/Program.cs
+++ ProjectManagement.API/Program.cs
@@ -60,11 +60,11 @@ builder.Services.AddSwaggerGen(c =>
 
 var app = builder.Build();
 
-// using (var scope = app.Services.CreateScope())
-// {
-//     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-//     db.Database.EnsureCreated(); // 或 db.Database.Migrate();
-// }
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.EnsureCreated(); // 或 db.Database.Migrate();
+}
 
 if (app.Environment.IsDevelopment())
 {
--- ProjectManagement.Application/DTOs/Projects/CreateProjectDto.cs
+++ ProjectManagement.Application/DTOs/Projects/CreateProjectDto.cs
@@ -2,7 +2,7 @@ namespace ProjectManagement.Application.DTOs.Projects
 {
     public class CreateProjectDto
     {
-        public string Name { get; set; } = string.Empty;
+        public string Title { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public string Status { get; set; } = "PLANNING"; // default
         public string Priority { get; set; } = "MEDIUM";  // default
