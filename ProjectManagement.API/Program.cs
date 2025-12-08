5a0dbd40de02c03bb2402d541726643a2053d320
jinny0122 <sheahjinny@gmail.com>
2025-12-08 17:54:30 +0800

    WIP: save local changes before switching branch

--- ProjectManagement.API/Program.cs
+++ ProjectManagement.API/Program.cs
@@ -1,3 +1,88 @@
+// using Microsoft.OpenApi.Models;
+// using Microsoft.EntityFrameworkCore;
+// using ProjectManagement.Application.Mapping;
+// using ProjectManagement.Domain.Interfaces.Repositories;
+// using ProjectManagement.Application.Interfaces.Services;
+// using ProjectManagement.Application.Services;
+// using ProjectManagement.Infrastructure.Data;
+// using ProjectManagement.Infrastructure.Repositories;
+// using AutoMapper;
+
+
+
+// var builder = WebApplication.CreateBuilder(args);
+
+// // Add DbContext
+// builder.Services.AddDbContext<AppDbContext>(options =>
+//     options.UseSqlServer(builder.Configuration.GetConnectionString("ProjectManagementDBConnectionString")));
+
+// // Add repositories
+// builder.Services.AddScoped<IUserRepository, UserRepository>();
+// builder.Services.AddScoped<IProjectRepository, ProjectRepository>();
+// builder.Services.AddScoped<IProjectTaskRepository, ProjectTaskRepository>();
+// builder.Services.AddScoped<ICommentRepository, CommentRepository>();
+// builder.Services.AddScoped<INotificationRepository, NotificationRepository>();
+
+// // Add services
+// builder.Services.AddScoped<IUserService, UserService>();
+// builder.Services.AddScoped<IProjectService, ProjectService>();
+// builder.Services.AddScoped<IProjectTaskService, ProjectTaskService>();
+// builder.Services.AddScoped<ICommentService, CommentService>();
+// builder.Services.AddScoped<INotificationService, NotificationService>();
+// builder.Services.AddScoped<IDashboardService, DashboardService>();
+
+// // Add AutoMapper
+// builder.Services.AddAutoMapper(
+//     typeof(Program),
+//  typeof(ProjectProfile),
+//   typeof(CollaborationProfile),
+//    typeof(ProjectTaskProfile),
+//     typeof(UserProfile)
+//     );
+
+
+
+// // Add controllers
+// builder.Services.AddControllers();
+
+// // Add Swagger
+// builder.Services.AddEndpointsApiExplorer();
+// builder.Services.AddSwaggerGen(c =>
+// {
+//     c.SwaggerDoc("v1", new OpenApiInfo
+//     {
+//         Title = "ProjectManagement API",
+//         Version = "v1",
+//         Description = "API for Project Management System"
+//     });
+// });
+
+
+// var app = builder.Build();
+
+// // using (var scope = app.Services.CreateScope())
+// // {
+// //     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+// //     db.Database.EnsureCreated(); // 或 db.Database.Migrate();
+// // }
+
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI(c =>
+//     {
+//         c.SwaggerEndpoint("/swagger/v1/swagger.json", "ProjectManagement API v1");
+//     });
+// }
+
+// app.UseHttpsRedirection();
+// app.UseAuthorization();
+
+// app.MapControllers();
+
+// app.Run();
+
+
 using Microsoft.OpenApi.Models;
 using Microsoft.EntityFrameworkCore;
 using ProjectManagement.Application.Mapping;
@@ -7,8 +92,6 @@ using ProjectManagement.Application.Services;
 using ProjectManagement.Infrastructure.Data;
 using ProjectManagement.Infrastructure.Repositories;
 using AutoMapper;
-
-
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -34,16 +117,23 @@ builder.Services.AddScoped<IDashboardService, DashboardService>();
 // Add AutoMapper
 builder.Services.AddAutoMapper(
     typeof(Program),
- typeof(ProjectProfile),
-  typeof(CollaborationProfile),
-   typeof(ProjectTaskProfile),
+    typeof(ProjectProfile),
+    typeof(CollaborationProfile),
+    typeof(ProjectTaskProfile),
     typeof(UserProfile)
-    );
-
-
+);
 
 // Add controllers
 builder.Services.AddControllers();
+
+// ===== 添加 CORS 配置 =====
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader());
+});
 
 // Add Swagger
 builder.Services.AddEndpointsApiExplorer();
@@ -57,14 +147,17 @@ builder.Services.AddSwaggerGen(c =>
     });
 });
 
-
 var app = builder.Build();
 
-// using (var scope = app.Services.CreateScope())
-// {
-//     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-//     db.Database.EnsureCreated(); // 或 db.Database.Migrate();
-// }
+// ===== 使用 CORS =====
+app.UseCors("AllowAll");
+
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.EnsureCreated(); // 或 db.Database.Migrate();
+}
 
 if (app.Environment.IsDevelopment())
 {
