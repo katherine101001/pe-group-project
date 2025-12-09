104603b588fd130f9ff99528ca8439af5532b325
katherine101001 <katherine.academic@gmail.com>
2025-12-09 20:57:55 +0800

    Done Project GET API for ALL Project in Project Page

---
+++ ProjectManagement.Infrastructure/Migrations/20251209124423_AddPriorityToProject.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectManagement.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPriorityToProject : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Priority",
+                table: "Project",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Priority",
+                table: "Project");
+        }
+    }
+}
