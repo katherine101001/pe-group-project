cfe6a769ee2e0e5ec667ca95cef0ca264cabb90a
Alex <jianshengalexng17317@gmail.com>
2025-12-11 15:04:14 +0800

    Update Latest Version with Database Upate, User Roles Updated, Register Controller

--- ProjectManagement.Application/Mapping/UserProfile.cs
+++ ProjectManagement.Application/Mapping/UserProfile.cs
@@ -15,9 +15,14 @@ namespace ProjectManagement.Application.Mapping
             // CreateMap<User,InviteTeamDto>();
 
             // Input DTO -> Entity
-            CreateMap<CreateUserDto, User>();
-            //    .ForMember(dest => dest.Role, opt => opt.Ignore());
+            CreateMap<CreateUserDto, User>()
+                .ForMember(dest => dest.Role, opt => opt.Ignore());
             CreateMap<InviteTeamDto, User>();
+            CreateMap<InviteTeamDto, User>()
+                .ForMember(dest => dest.RoleId, opt => opt.Ignore()) // RoleId 需要在 Service 中手动设置
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => "New User")) // 可以给默认名字
+                .ForMember(dest => dest.Password, opt => opt.MapFrom(src => "12345")); // 默认密码
+
             //CreateMap<UpdateUserDto, User>();
 
         }
